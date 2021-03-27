9c52eafb7f23bede0afae34d807769caa452bbb4
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2021-03-27 20:30:27 +0900

    resolve

--- src/bin/abc081b.rs
+++ src/bin/abc081b.rs
@@ -1,3 +1,25 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
 fn main() {
-    todo!();
+    let n = read();
+    println!(
+        "{}",
+        (0..n)
+            .map(|_| read::<u32>().trailing_zeros())
+            .min()
+            .unwrap()
+    );
 }
